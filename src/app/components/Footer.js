f1569a37242812a472beedce8b186a5ac4ea9cd6
Sh Flávio <flaviohps8@gmail.com>
2024-07-02 16:14:20 -0300

    Update Footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,7 +4,7 @@ import '../../styles/Footer.css'
 const Footer = () => {
     return (
         <footer className="footer">
-            <small className="footerNotice">Copyright (c) 2024 Sh Flávio <a href="https://github.com/Fh-Shadow/portfolio/blob/main/public/humans.txt">See more</a>.</small>
+            <small className="footerNotice">Copyright (c) 2024 <a href="https://github.com/Fh-Shadow/portfolio/blob/main/public/humans.txt">See more</a>.</small>
         </footer>
     )
 }
