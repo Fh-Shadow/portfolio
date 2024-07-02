c3294ecfd7d641b7f9647ff1b0a03411d2413d2f
Sh Flávio <flaviohps8@gmail.com>
2024-07-02 18:42:24 -0300

    Update icons.js

--- src/app/icons.js
+++ src/app/icons.js
@@ -1,11 +1,11 @@
-import { addIconsIon } from 'ionicons'
+import { addIcons } from 'ionicons'
 import { logoHtml5, logoCss3 } from 'ionicons/icons'
 import * as allIcons from 'ionicons/icons'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPhp } from '@fortawesome/free-brands-svg-icons'
 
-addIconsIon({
+addIcons({
     logoHtml5,
     logoCss3,
     ...allIcons,
