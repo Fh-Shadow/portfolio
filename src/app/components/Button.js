c4fa872e96b8322629c4685db429cd22aa18930b
Sh Flávio <flaviohps8@gmail.com>
2024-07-02 11:58:16 -0300

    Update Project

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,55 +1,59 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import '../../styles/Button.css';
+import '../../styles/Button.css'
 
 function Button({ icon, label, type = "secondary", redirect, action }) {
+  // Actions & Other Logics
   const handleClick = () => {
-    // Verificar se o botão está desabilitado
+    // Cancel all if disabled
     if (type.includes('disabled')) {
-      return;
+      return
     }
 
+    // Redirect with two parameters
     if (redirect) {
-      const { url, blank } = redirect;
+      const { url, blank } = redirect
       if (blank) {
-        window.open(url, '_blank');
+        window.open(url, '_blank')
       } else {
-        window.location.href = url;
+        window.location.href = url
       }
     }
 
     if (action) {
-      const { type: actionType, target, condition } = action;
+      const { type: actionType, target, condition } = action
       if (!condition || condition()) {
+        // Action list cases
         switch (actionType) {
-          case 'disable':
-            document.querySelector(target).disabled = true;
-            break;
-          case 'enable':
-            document.querySelector(target).disabled = false;
-            break;
-          case 'send':
+          case 'disable': // Disable something
+            document.querySelector(target).disabled = true
+            break
+          case 'enable': // Enable something
+            document.querySelector(target).disabled = false
+            break
+          case 'send': // Send something
             fetch(target, { method: 'POST' });
-            break;
-          case 'pull':
-            fetch(target).then(response => response.json());
-            break;
-          case 'refresh':
-            window.location.reload();
-            break;
-          case 'change':
-            document.querySelector(target).textContent = action.content;
-            break;
-          default:
-            console.warn('Unknown action type');
+            break
+          case 'pull': // Pull something
+            fetch(target).then(response => response.json())
+            break
+          case 'refresh': // Refresh something
+            window.location.reload()
+            break
+          case 'change': // Change something
+            document.querySelector(target).textContent = action.content
+            break
+          default: // Error no parameters provided
+            console.warn('Unknown action type')
         }
       }
     }
   };
 
+  // Icon & Label
   if (!icon && !label) {
-    throw new Error('Either <icon> or <label> must be provided');
+    throw new Error('Either <icon> or <label> must be provided')
   }
 
   return (
@@ -57,9 +61,10 @@ function Button({ icon, label, type = "secondary", redirect, action }) {
       {icon && <div className="buttonIcon">{icon}</div>}
       {label && <div className="buttonLabel">{label}</div>}
     </button>
-  );
+  )
 }
 
+// Button list of props
 Button.propTypes = {
   icon: PropTypes.node,
   label: PropTypes.string,
@@ -83,8 +88,8 @@ Button.propTypes = {
     type: PropTypes.oneOf(['disable', 'enable', 'send', 'pull', 'refresh', 'change']).isRequired,
     target: PropTypes.string.isRequired,
     condition: PropTypes.func,
-    content: PropTypes.string, // Specific for 'change' action
+    content: PropTypes.string,
   }),
-};
+}
 
-export default Button;
+export default Button
